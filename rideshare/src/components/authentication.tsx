ebf6512a898e4c604f40b2ed6b29e212589672f4
Hanish Rishen <hanishrishen@gmail.com>
2024-07-27 09:51:08 +0530

    name added

--- rideshare/src/components/authentication.tsx
+++ rideshare/src/components/authentication.tsx
@@ -20,24 +20,23 @@ export default function AuthenticationPage() {
   return (
     <>
       <div>
-      <iframe
-            src="https://lottie.host/embed/d1abe612-14e9-47a9-bc73-fbb22b8fa852/D0QFNbwpcF.json"
-            className="absolute inset-0 w-full h-full border-0"
-            allowFullScreen
-          />
+        <iframe
+          src="https://lottie.host/embed/d1abe612-14e9-47a9-bc73-fbb22b8fa852/D0QFNbwpcF.json"
+          className="absolute inset-0 w-full h-full border-0"
+          allowFullScreen
+        />
       </div>
       <div className="container relative flex h-full flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0">
         <div className="relative hidden h-full flex-col bg-muted p-10 text-black dark:border-r lg:flex">
-        <div className="flex items-center justify-center w-full h-full">
-  <iframe
-    src="https://lottie.host/embed/5ad493df-451a-4d69-94ae-a156191cd5c4/kpPkwmNZrT.json"
-    className="w-[500px] h-[500px] border-0"
-    allowFullScreen
-  />
-</div>
-
           <div className="relative z-20 flex items-center text-lg font-medium">
             RideShare.
+          </div>
+          <div className="flex items-center justify-center w-full h-full">
+            <iframe
+              src="https://lottie.host/embed/5ad493df-451a-4d69-94ae-a156191cd5c4/kpPkwmNZrT.json"
+              className="w-[500px] h-[500px] border-0"
+              allowFullScreen
+            />
           </div>
           <div className="relative z-20 mt-auto">
             <blockquote className="space-y-2">
