0f7649d90f31457c285145799a6f7b9340c15fcd
anaypurohit0907 <ap1851@srmist.edu.in>
2024-07-27 10:41:51 +0530

    logic file

---
+++ rideshare/src/app/dblogic.tsx
@@ -0,0 +1,9 @@
+import { supabase } from "@/lib/supabaseClient";
+
+function addReq(start_loc: string, end_loc: string, user_id: string) {}
+
+async function pair_users(
+  start_loc: string,
+  end_loc: string,
+  user_id: string
+) {}
